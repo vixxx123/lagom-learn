b15cede87ea4187b03dab85e24b582f5f6ab31e9
Wiktor Tychulski <wiktor.tychulski@img.com>
2017-02-01 00:17:43 +0000

    changing greeting msg affects all users, add support for changing greeting msg via stream api

--- lagom-learn-impl/src/main/scala/com/codelouders/learn/lagom/lagomlearn/impl/LagomlearnServiceImpl.scala
+++ lagom-learn-impl/src/main/scala/com/codelouders/learn/lagom/lagomlearn/impl/LagomlearnServiceImpl.scala
@@ -11,15 +11,14 @@ class LagomlearnServiceImpl(persistentEntityRegistry: PersistentEntityRegistry) 
 
   override def hello(id: String) = ServiceCall { _ =>
     // Look up the lagom-learn entity for the given ID.
-    val ref = persistentEntityRegistry.refFor[LagomlearnEntity](id)
-
+    val ref = persistentEntityRegistry.refFor[LagomlearnEntity]("greet")
     // Ask the entity the Hello command.
     ref.ask(Hello(id, None))
   }
 
   override def useGreeting(id: String) = ServiceCall { request =>
     // Look up the lagom-learn entity for the given ID.
-    val ref = persistentEntityRegistry.refFor[LagomlearnEntity](id)
+    val ref = persistentEntityRegistry.refFor[LagomlearnEntity]("greet")
 
     // Tell the entity to use the greeting message specified.
     ref.ask(UseGreetingMessage(request.message))
--- lagom-learn-stream-impl/src/main/scala/com/codelouders/learn/lagom/lagomlearnstream/impl/LagomlearnStreamServiceImpl.scala
+++ lagom-learn-stream-impl/src/main/scala/com/codelouders/learn/lagom/lagomlearnstream/impl/LagomlearnStreamServiceImpl.scala
@@ -1,8 +1,9 @@
 package com.codelouders.learn.lagom.lagomlearnstream.impl
 
+import akka.stream.scaladsl.Keep
 import com.lightbend.lagom.scaladsl.api.ServiceCall
 import com.codelouders.learn.lagom.lagomlearnstream.api.LagomlearnStreamService
-import com.codelouders.learn.lagom.lagomlearn.api.LagomlearnService
+import com.codelouders.learn.lagom.lagomlearn.api.{GreetingMessage, LagomlearnService}
 
 import scala.concurrent.Future
 
@@ -11,6 +12,14 @@ import scala.concurrent.Future
   */
 class LagomlearnStreamServiceImpl(lagomlearnService: LagomlearnService) extends LagomlearnStreamService {
   def stream = ServiceCall { hellos =>
-    Future.successful(hellos.mapAsync(8)(lagomlearnService.hello(_).invoke()))
+    Future.successful(hellos.mapAsync(8){ command ⇒
+      if (command.startsWith("change:")){
+        val id = command.split(":")(1)
+        lagomlearnService.useGreeting(id).invoke(request = GreetingMessage(id))
+        Future.successful("Done")
+      } else {
+        lagomlearnService.hello(command).invoke()
+      }
+    })
   }
 }
